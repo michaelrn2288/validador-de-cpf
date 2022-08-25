10d62dc712409f21e0b8653faeaaccb0589ea4fd
Michael Rocha Nunes <michaelrn2288@gmail.com>
2022-08-25 16:11:17 -0300

    restructure code and add all functionalities

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,94 +1,128 @@
-function validateCpf(cpf) {
-    const cpfCleaned = cpf.replace(/\D+/g, '')
+cpfValidator()
 
-    if (cpfCleaned.length !== 11) return console.log('cpf must have 11 digits')
+function cpfValidator() {
+    const result = document.querySelector('#result')
+    let cpfValidated = []
 
-    const cpfArray = Array.from(cpfCleaned)
-    const cpfWithoutDigit = cpfArray.splice(0, 9)
-    const cpfFirstDigit = calcDigit(cpfWithoutDigit, 10)
-    const cpfWithOneDigit = [...cpfWithoutDigit, cpfFirstDigit]
-    const cpfSecondDigit = calcDigit(cpfWithOneDigit, 11)
-    const cpfValidated = [...cpfWithOneDigit, cpfSecondDigit].join('')
+    inputResponse()
 
-    cpfValidated === cpfCleaned ? console.log('valid cpf number') : console.log('invalid cpf number')
+    function validateCpf(cpf) {
+        const cpfCleaned = cpf.replace(/\D+/g, '')
 
-    function calcDigit(cpfPiece, numMultiplier) {
-        const cpfOperation = cpfPiece
-            .map((value, index) => Number(value) * (numMultiplier - index))
-            .reduce((accumulator, value) => accumulator + value, 0)
+        if (cpfCleaned.length !== 11) return result.innerText = 'o CPF deve ter 11 dígitos'
 
-        const digit = 11 - (cpfOperation % 11)
-        return digit < 10 ? digit : 0
+        const cpfArray = Array.from(cpfCleaned)
+        const cpfWithoutDigit = cpfArray.splice(0, 9)
+
+        buildValidCpf(cpfWithoutDigit)
+
+        cpfValidated === cpfCleaned ? result.innerText = 'CPF válido' : result.innerText = 'CPF inválidor'
     }
-}
 
-validateCpf('705.484.450-5')
-
-chooseActionOption()
-
-function chooseActionOption() {
-    const chosenOptionContainer = document.querySelector('#chosen-option-container')
-    const actionOption = document.querySelector('#cpf-option')
-
-    document.addEventListener('load', () => optionValidateCpf())
-
-    actionOption.addEventListener('input', () => {
-        switch (actionOption.value) {
-            case 'validate-cpf':
-                optionValidateCpf()
-                break;
-
-            case 'generate-random-cpf':
-                optionGenerateRandomCpf()
-                break;
-
-            case 'create-manual-cpf':
-                optionCreateManualCpf()
+    function generateRandomCpf() {
+        const cpfWithoutDigit = []
+        for (let i = 0; i < 9; i++) {
+            cpfWithoutDigit[i] = Math.floor(Math.random() * 10)
         }
+        buildValidCpf(cpfWithoutDigit)
+        result.textContent = cpfValidated
+    }
+
+    function createManualCpf(cpf) {
+        const cpfCleaned = cpf.replace(/\D+/g, '')
+
+        if (cpfCleaned.length !== 9) return result.innerText = 'you must insert 9 digits'
+
+        const cpfArray = Array.from(cpfCleaned)
+        const cpfWithoutDigit = cpfArray.splice(0, 9)
+
+        buildValidCpf(cpfWithoutDigit)
+        result.textContent = cpfValidated
+    }
+
+    function buildValidCpf(cpfWithoutDigit) {
+        const cpfFirstDigit = calcDigit(cpfWithoutDigit, 10)
+        const cpfWithOneDigit = [...cpfWithoutDigit, cpfFirstDigit]
+        const cpfSecondDigit = calcDigit(cpfWithOneDigit, 11)
+        cpfValidated = [...cpfWithOneDigit, cpfSecondDigit].join('')
+
+        function calcDigit(cpfPiece, numMultiplier) {
+            const cpfOperation = cpfPiece
+                .map((value, index) => Number(value) * (numMultiplier - index))
+                .reduce((accumulator, value) => accumulator + value, 0)
+
+            const digit = 11 - (cpfOperation % 11)
+            return digit < 10 ? digit : 0
+        }
+    }
+
+    function inputResponse() {
+        const chosenOptionContainer = document.querySelector('#chosen-option-container')
+        const actionOption = document.querySelector('#cpf-option')
+        const textOrientation = chosenOptionContainer.querySelector('#text-orientation')
+        const cpfInput = chosenOptionContainer.querySelector('#cpf-input')
+        const resultButton = chosenOptionContainer.querySelector('#result-button')
+
+        actionOption.addEventListener('input', () => {
+            switch (actionOption.value) {
+                case 'validate-cpf':
+                    optionValidateCpf()
+                    break
+
+                case 'generate-random-cpf':
+                    optionGenerateRandomCpf()
+                    break
+
+                case 'create-manual-cpf':
+                    optionCreateManualCpf()
+            }
+        })
+
+        resultButton.addEventListener('click', () => {
+            switch (resultButton.value) {
+                case 'btn-validate-cpf':
+                    validateCpf(cpfInput.value)
+                    break
+
+                case 'btn-generate-cpf':
+                    generateRandomCpf()
+                    break
+
+                case 'btn-create-cpf':
+                    createManualCpf(cpfInput.value)
+            }
+        })
 
         function optionValidateCpf() {
-            chosenOptionContainer.innerHTML = ''
-            const orientationText1 = createElementWithText('h3', 'insira o CPF a ser validado')
-            const cpfInput1 = document.createElement('input')
-            cpfInput1.setAttribute('type', 'text')
-            cpfInput1.setAttribute('maxlength', '11')
-            const resultButton1 = createElementWithText('button', 'validar CPF')
-            appendElements(chosenOptionContainer, orientationText1, cpfInput1, resultButton1)
-            console.log('test')
+            textOrientation.innerText = 'insira o CPF a ser validado'
+            cpfInput.removeAttribute('hidden')
+            cpfInput.removeAttribute('maxlength')
+            cpfInput.setAttribute('maxlength', 11)
+            resultButton.innerText = 'validar CPF'
+            resultButton.removeAttribute('value')
+            resultButton.setAttribute('value', 'btn-validate-cpf')
+            result.innerText = ''
         }
 
         function optionGenerateRandomCpf() {
-            chosenOptionContainer.innerHTML = ''
-            const resultButton2 = createElementWithText('button', 'gerar CPF')
-            chosenOptionContainer.appendChild(resultButton2)
+            textOrientation.innerText = ''
+            cpfInput.setAttribute('hidden', 'hidden')
+            resultButton.innerText = 'gerar CPF'
+            resultButton.removeAttribute('value')
+            resultButton.setAttribute('value', 'btn-generate-cpf')
+            result.innerText = ''
         }
 
         function optionCreateManualCpf() {
-            chosenOptionContainer.innerHTML = ''
-            const orientationText2 = createElementWithText('h3',
-                'insira os primeiros nove números do CPF para ele ser completado com dígitos válidos')
-            const cpfInput2 = document.createElement('input')
-            cpfInput2.setAttribute('type', 'text')
-            cpfInput2.setAttribute('maxlength', '9')
-            const resultButton3 = createElementWithText('button', 'criar CPF')
-            appendElements(chosenOptionContainer, orientationText2, cpfInput2, resultButton3)
-            console.log('test')
+            textOrientation.innerText =
+                'insira os primeiros nove números do CPF para ele ser completado com dígitos válidos'
+            cpfInput.removeAttribute('hidden')
+            cpfInput.removeAttribute('maxlength')
+            cpfInput.setAttribute('maxlength', 9)
+            resultButton.innerText = 'criar CPF'
+            resultButton.removeAttribute('value')
+            resultButton.setAttribute('value', 'btn-create-cpf')
+            result.innerText = ''
         }
-    })
-
-    function appendElements(parent, child1, child2) {
-        for (let element of arguments) {
-            console.log(element)
-            if (element !== parent) {
-                parent.appendChild(element)
-            }
-        }
-    }
-
-    function createElementWithText(tag, text) {
-        const element = document.createElement(tag)
-        const elementText = document.createTextNode(text)
-        element.appendChild(elementText)
-        return element
     }
 }
